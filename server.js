18d28420dddae3fb9b4bbbe935b4671dba8d0643
rohitsable123 <138371758+rohitsable123@users.noreply.github.com>
2025-04-16 17:26:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
